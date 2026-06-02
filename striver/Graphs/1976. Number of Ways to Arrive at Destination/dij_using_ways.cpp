07144315e4b6e34ea2d173610904ad89bfcaa332
Rsan25 <roshan25122005@gmail.com>
2026-06-02 18:04:44 +0530

    used a ways array to solve it

---
+++ striver/Graphs/1976. Number of Ways to Arrive at Destination/dij_using_ways.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        vector<pair<int,int>> adj[n];
+
+        for(auto it : roads){
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+
+        vector<long long> dist(n, 1e18);
+        vector<long long> ways(n, 0);
+
+        dist[0] = 0;
+        ways[0] = 1;
+
+        priority_queue<
+            pair<long long,int>,
+            vector<pair<long long,int>>,
+            greater<pair<long long,int>>
+        > pq;
+
+        pq.push({0,0});
+
+        int mod = 1e9 + 7;
+
+        while(!pq.empty()){
+            long long d = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            if(d > dist[node]) continue;
+
+            for(auto next : adj[node]){
+                int v = next.first;
+                long long w = next.second;
+
+                if(d + w < dist[v]){
+                    dist[v] = d + w;
+                    ways[v] = ways[node];
+                    pq.push({dist[v], v});
+                }
+                else if(d + w == dist[v]){
+                    ways[v] = (ways[v] + ways[node]) % mod;
+                }
+            }
+        }
+
+        return ways[n-1] % mod;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    int n = 7;
+
+    vector<vector<int>> roads = {
+        {0,6,7},
+        {0,1,2},
+        {1,2,3},
+        {1,3,3},
+        {6,3,3},
+        {3,5,1},
+        {6,5,1},
+        {2,5,1},
+        {0,4,5},
+        {4,6,2}
+    };
+
+    cout << "Number of shortest paths = "
+         << sol.countPaths(n, roads)
+         << endl;
+
+    return 0;
+}
