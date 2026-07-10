ec54d7f63fcb2fad55ac1f6b0c076982c49bd0fa
Roshan Mishra <roshan25122005@gmail.com>
2026-07-10 11:00:43 +0530

    Time: 0 ms (100%), Space: 12 MB (6.3%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(int i,int j,vector<vector<int>>& dp, vector<vector<int>>& grid ){
+        if(grid[i][j] == 1) return 0;  // if current cell is blocked
+        if(i == 0 && j == 0) return 1;  // base case
+        if(dp[i][j]!=-1) return dp[i][j];
+        int l =0, u =0;
+
+        if(i-1>=0) l = solve(i-1,j,dp,grid);
+        if(j-1>=0) u = solve(i,j-1,dp,grid);
+
+        return dp[i][j]=l + u;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
+        int m = Grid.size();
+        int n = Grid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,dp,Grid);
+    }
+};
