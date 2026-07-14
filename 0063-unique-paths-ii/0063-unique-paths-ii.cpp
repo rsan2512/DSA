ad357b19b649a59a12cb81759f394db12ac3dff3
Roshan Mishra <roshan25122005@gmail.com>
2026-07-14 17:58:19 +0530

    Time: 3 ms (9.89%), Space: 11.8 MB (72.62%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<vector<int>>& dp, vector<vector<int>>& grid ){
-        if(grid[i][j] == 1) return 0;  // if current cell is blocked
-        if(i == 0 && j == 0) return 1;  // base case
-        if(dp[i][j]!=-1) return dp[i][j];
-        int l =0, u =0;
-
-        if(i-1>=0) l = solve(i-1,j,dp,grid);
-        if(j-1>=0) u = solve(i,j-1,dp,grid);
-
-        return dp[i][j]=l + u;
+    int solve(int m,int n,vector<vector<int>>& dp, vector<vector<int>>& grid ){
+        for(int i =0;i<m ;i++){
+            for(int j = 0;j<n;j++){
+                if(i==0 && j ==0) dp[i][j]=1;
+                else if(grid[i][j]==1) dp[i][j] = 0;
+                else{
+                    int up =0,left = 0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = up+left;
+                }
+            }
+        }return dp[m-1][n-1];
     }
     int uniquePathsWithObstacles(vector<vector<int>>& Grid) {
         int m = Grid.size();
         int n = Grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,dp,Grid);
+        if(Grid[m-1][n-1]==1) return 0;
+        if(Grid[0][0]==1) return 0;
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        return solve(m,n,dp,Grid);
     }
 };
