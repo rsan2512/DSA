09ceaba7ea48802c05d1d09307b9f1e6c22920dc
Roshan Mishra <roshan25122005@gmail.com>
2026-07-19 18:13:55 +0530

    Time: 133 ms (52.12%), Space: 13.5 MB (81.27%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -3,14 +3,12 @@ public:
 
     bool isSubsetSum(vector<int>& arr, int sum) {
         int n = arr.size();
-        vector<bool> prev(sum + 1,0);
+        vector<bool> prev(sum + 1,0), curr(sum + 1,0);
         
-        prev[0] = true;
+        prev[0]=curr[0]= true;
         if (arr[0] <= sum) prev[arr[0]] = true;
         
         for(int i = 1;i<n;i++){
-            vector<bool> curr(sum + 1,0);
-            curr[0] = true;
             for(int tar = 1;tar<=sum;tar++){
                 bool notTake = prev[tar];
                 
